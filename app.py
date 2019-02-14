b8eaed3aa773428764e0e5d4c5858ebf32d48921
Wataru Inoue <southavem@gmail.com>
2019-02-14 23:25:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,19 @@ class Router:
                 return r['callback'], url_vars
             
         return error_callback, {}
+
+class App:
+    def __init__(self):
+        self.router = Router()
+
+    def route(self, path=None, method='GET', callback=None):
+        def decorator(callback_func):
+            self.router.add(method, path, callback_func)
+            return callback_func
+        return decorator(callback) if callback else decorator
+    
+    def __call__(self, env, start_response):
+        method = env['REQUEST_METHOD'].upper()
+        path = env['PATH_INFO'] or '/'
+        callback, kwargs = self.router.match(method, path)
+        return callback(env, start_response, **kwargs)
