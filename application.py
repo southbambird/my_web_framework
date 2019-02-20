db58e0cd453622c51fff1ce37706871cab40dda7
Wataru Inoue <southavem@gmail.com>
2019-02-20 14:16:17 +0900

    Update application.py

--- application.py
+++ application.py
@@ -8,23 +8,11 @@ def hello(request, start_response):
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
     return [b'Hello World']
 
-@app.route('^/user/$', 'POST')
-def create_user(request, start_response):
-    start_response('201 Created', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'User Created']
-
-@app.route('^/user/(?P<name>\w+)/$', 'GET')
+@app.route('^/user/(?P<name>\w+)$', 'GET')
 def user_detail(request, start_response, name):
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
     body = 'Hello {name}'.format(name=name)
     return [body.encode('utf-8')]
-
-@app.route('^/user/(?P<name>\w+)/follow/$', 'POST')
-def create_user(request, start_response, name):
-    start_response('201 Created', [('Content-type', 'text/plain; charset=utf-8')])
-    body = 'Followed {name}'.format(name=name)
-    return [body.encode('utf-8')]
-
 
 if __name__ == '__main__':
     httpd = make_server('', 8000, app)
